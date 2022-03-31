1c62fa729cd78a89df4ba837084a1d9db61d2ad5
Sara Fernández Arias <uo269546@uniovi.es>
2022-03-31 17:54:13 +0200

    SDI–2122-307-7.101-Ejercicio complementario 1

---
+++ musicstoreapp/routes/authors.js
@@ -0,0 +1,52 @@
+module.exports = function (app,twig) {
+    app.get("/authors/", function (req, res) {
+
+        let authors=[{
+            "name": "Flea",
+            "group": "Red hot chilli pepers",
+            "role":"Bass player"
+        },{
+            "name": "Anthony Kiedis",
+            "group": "Red hot chilli pepers",
+            "role":"singer"
+        }, {
+            "name": "John Frusciante",
+            "group": "Red hot chilli pepers",
+            "role":"guitarrist"
+        }];
+        let response=
+            {
+                seller:'tienda de canciones',
+                authors:authors
+            }
+        res.render("authors/authors.twig",response);
+    });
+
+    app.get('/authors/add', function (req, res) {
+        res.render("authors/add.twig");
+    });
+
+    app.post('/authors/add', function (req, res) {
+        let response = "Autor añadido:";
+        if (req.query.name == null && typeof (req.query.name) == "undefined")
+            response += '< name > no envíado en la petición. <br>';
+        else
+            response += req.query.name + '<br>';
+        response += "Grupo:"
+        if (req.query.group == null && typeof (req.query.group) == "undefined")
+            response += '< group > no envíado en la petición. <br>';
+        else
+            response += req.query.group + '<br>';
+        response += "Rol:"
+        if (req.query.role == null && typeof (req.query.role) == "undefined")
+            response += '< role > no envíado en la petición. <br>';
+        else
+            response += req.query.role + '<br>';
+        res.send(response);
+    });
+
+    app.get('/authors/*', function (req, res) {
+        req.redirect("/authors/");
+    });
+
+};
